10a76a8ef00a719b483457f76f9e0f84b1681c95
tsny <tsny700@gmail.com>
2017-07-24 21:06:42 -0500

    xcode init

---
+++ Final4001/Final4001/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Final4001
+//
+//  Created by Taylor Snyder on 7/24/17.
+//  Copyright © 2017 tsny inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
